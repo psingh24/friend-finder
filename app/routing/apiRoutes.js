d9ee4479efe0e4bc7b01a759de7f996ba24d81d2
Prabhdeep <ps591990@gmail.com>
2017-07-16 01:45:30 -0500

    update

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,7 +6,16 @@ module.exports = function(app) {
 
     app.get("/api/friends", function(req, res) {
         res.json(friendsData);
-    })
+    });
 
+    app.post("/api/friends", function(req, res) {
+        var newFriend = req.body;
+        friendsData.push(newFriend);
+    });
 
+    //just so I can clear table while testing
+    app.get("/api/cleartables", function(req, res) {
+        friendsData = [];
+        
+});
 };
