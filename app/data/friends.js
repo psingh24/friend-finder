eac2221a0d25e60fc965b950e64e725a7cb8f568
Prabhdeep <ps591990@gmail.com>
2017-07-18 21:47:39 -0500

    update Html and add canvas for fun

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,8 +1,13 @@
 var friendsArray = [
   {
-    name: "Ahmed",
-    photo: "https://static.pexels.com/photos/126407/pexels-photo-126407.jpeg",
+    name: "Neo",
+    photo: "https://static.superdeluxe.com/dankland/generators/neo.jpg",
     scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+  },
+  {
+    name: "Mr. Anderson",
+    photo: "https://static.giantbomb.com/uploads/original/15/155745/2179153-18079_26016.gif",
+    scores: [1, 2, 4, 2, 2, 4, 5, 2, 3, 2]
   }
 ];
 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,47 +1,41 @@
 var friendsData = require("../data/friends");
 
-// console.log(friendsData)
 
 module.exports = function(app) {
+  app.get("/api/friends", function(req, res) {
+    res.json(friendsData);
+  });
 
-    app.get("/api/friends", function(req, res) {
-        res.json(friendsData);
-    });
+  app.post("/api/friends", function(req, res) {
+    var newFriend = req.body;
 
-    app.post("/api/friends", function(req, res) {
-        var newFriend = req.body;
-       
-    // console.log("hello")
-   
+    var total = 0;
 
-    var total = [];
+    var match = {
+      name: "",
+      photo: "",
+      difference: 1000
+    };
 
+    for (var i = 0; i < friendsData.length; i++) {
+      console.log(friendsData[i].scores);
+      total = 0;
+      for (var j = 0; j < friendsData[i].scores.length; j++) {
+        total += Math.abs(friendsData[i].scores[j] - newFriend.scores[j]);
 
-        for (var i=0; i< friendsData.length; i++) {
-           for (var j=0; j<friendsData[i].scores.length; j++) {
-               total.push(Math.abs(friendsData[i].scores[j] - newFriend.scores[j]))
-           }
-           
-          
+        if (total <= match.difference) {
+          match.name = friendsData[i].name;
+          match.photo = friendsData[i].photo;
+          match.difference = total;
         }
-        // var match = {
-        //     name: friendsData[0].name,
-        //     photo: friendsData[0].photo
-        // }
-        
-        // console.log(match)
+      }
+    }
+    friendsData.push(newFriend);
+    res.json(match);
+  });
 
-       
-
-         friendsData.push(newFriend);
-          res.json(match);
-
-
-    });
-
-    //just so I can clear table while testing
-    app.get("/api/cleartables", function(req, res) {
-        friendsData = [];
-        
-});
+  //just so I can clear table while testing
+  app.get("/api/cleartables", function(req, res) {
+    friendsData = [];
+  });
 };
