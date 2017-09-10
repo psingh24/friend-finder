1f61142974ea80595821eddaf8427ca69ba3963b
Prabhdeep <ps591990@gmail.com>
2017-09-09 19:18:57 -0500

    update readme

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ var path = require('path');
 var app = express();
 var PORT = process.env.PORT || 3000;
 
+app.use(express.static('app/public'));
+
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
